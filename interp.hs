cfdee1f597305a9b0f7091440f74ffc7fc6f8645
João Rebelo Pires <joaorgpires@gmail.com>
2017-12-10 20:19:40 +0000

    Adding beta reduction, examples, and "pretty" printing

--- interp.hs
+++ interp.hs
@@ -3,9 +3,31 @@
 data Expr = Var Char
     | Lam Char Expr
     | App Expr Expr
-    deriving (Show)
+
+-- "Pretty" printing
+instance Show Expr where
+    show (Var v)    = [v]
+    show (Lam x m) = "(\\" ++ [x] ++ "." ++ (show m) ++ ")"
+    show (App m n) = "(" ++ (show m) ++ " " ++ (show n) ++ ")"
 
 subst :: Expr -> Expr -> Expr -> Expr
 subst (Var x) l (Var y) = if x == y then l else (Var x)
 subst (Lam x m) l (Var y) = if x == y then (Lam x m) else (Lam x (subst m l (Var y)))
 subst (App m n) l (Var y) = App (subst m l (Var y)) (subst n l (Var y))
+
+-- Application case for beta reduction
+appRedex :: Expr -> Expr -> Expr
+appRedex (Lam x m) n = normalize (subst m n (Var x))
+appRedex m n = App (normalize m) (normalize n)
+
+-- Normalize the term
+normalize :: Expr -> Expr
+normalize (Var x) = Var x
+normalize (Lam x m) = Lam x (normalize m)
+normalize (App m n) = appRedex m n
+
+-- Examples
+ex1 = Lam 'x' (App (Var 'a') (Var 'x'))
+ex2 = Lam 'y' (App (Var 'b') (Var 'y'))
+ex3 = App ex2 (Var 'c')
+ex4 = App ex1 ex3
